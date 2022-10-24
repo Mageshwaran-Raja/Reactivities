18bf4b03ccca66f2484b19e66215527b4c13ffdd
mageshwaran16 <mageshwaran.raja@kanini.com>
2022-10-24 21:23:26 +0530

    End of Section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,8 @@
+using Application.Activities;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using Application.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +18,12 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(opt => 
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection")));
 
+
+builder.Services.AddMediatR(typeof(List.Handler).Assembly);
+
 builder.Services.AddCors();
+
+builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
 
 var app = builder.Build();
 
@@ -33,6 +41,7 @@ catch(Exception ex) {
     var logger = services.GetRequiredService<ILogger<Program>>();
     logger.LogError(ex, "Error occured during migration");
 }
+
 
 
 
